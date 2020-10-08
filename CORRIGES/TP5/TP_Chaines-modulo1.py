466e6be0c894910a07848d05b5a82ecc9d3628b3
thfruchart <66477650+thfruchart@users.noreply.github.com>
2020-10-08 11:33:13 +0200

    Add files via upload

---
+++ CORRIGES/TP5/TP_Chaines-modulo1.py
@@ -0,0 +1,13 @@
+﻿# parcours d'une chaîne avec saut
+
+chaine = 'ABCDE'
+long = len (chaine)
+saut = 2
+indice = 0
+resu = ''
+
+for i in range(long):
+    resu = resu + chaine[indice]
+    indice = (indice + saut) % long
+
+print(resu)
---
+++ CORRIGES/TP5/TP_Chaines-modulo3.py
@@ -0,0 +1,27 @@
+﻿# parcours d'une chaîne avec saut
+
+chaine = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+long = len (chaine)
+
+
+for saut in range(long):
+    print(saut)
+    indice = 0
+    resu = ''
+    for i in range(long):
+        resu = resu + chaine[indice]
+        indice = (indice + saut) % long
+    print(resu)
+    test = True
+    i = 1
+    while test and i<long:
+        if resu[i]==chaine[0]:
+            test = False
+        i = i+1
+
+    if test:
+        print("tous les caractères sont présents")
+    else:
+        print('Problème : certains caractères sont répétés !')
+    print()
+
