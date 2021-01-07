431d032e2a4383a638a1e33f1a9391703a6ffd66
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-01-07 09:28:43 +0100

    Add files via upload

---
+++ TP_Calendrier_A_COMPLETER.py
@@ -0,0 +1,29 @@
+﻿#TP Calendrier à  compléter
+
+Mois=['janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+      'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+
+LongueurMois=[31,28,31,30,31,30,31,31,30,31,30,31]
+
+RangFinMois=[31,0,0,0,0,0,0,0,0,0,0,0]
+
+for i in range(1,12):  # 12 exclu, 11 inclus
+    RangFinMois[i]=LongueurMois[i]+RangFinMois[i-1]
+
+
+def Date(n): #n est un entier compris entre 1 et 365
+    i=0
+    while n> RangFinMois[i]:
+        i=i+1
+    #WhileEnd
+    m= Mois[i]
+
+    j=n-RangFinMois[i-1]
+
+    resu =  str(j) +' '+m
+    return resu
+
+
+def Rang(j, m ):
+    return 'Fonction à  compléter'
+
