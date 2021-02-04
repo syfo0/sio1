6466ec5e54c4d780bfc56ef089675ac3505762ff
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-02-04 10:51:29 +0100

    Add files via upload

---
+++ TP13/TP13bis_A_COMPLÉTER.py
@@ -0,0 +1,23 @@
+﻿# TP13
+
+def valeur(car):
+    """convertit au format entier un caractère compris entre 0 et F"""
+    chiffre_list = '0123456789ABCDEF'
+    resu = -1
+    for i in range(len(chiffre_list)):
+        if chiffre_list[i]==car :
+            resu = i
+    return resu
+
+def convert (chaine):
+    long = len (chaine)
+    resu = 0
+    expo = long - 1
+    for i in range (long):
+        chiffre = valeur(chaine[i])
+        print(chiffre)
+        print(expo)
+        expo = expo -1
+        
+    return resu
+
