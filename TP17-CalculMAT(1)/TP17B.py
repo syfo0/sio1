ca8aa817cfdf02d8a943bdc7919591e85046677a
thfruchart <66477650+thfruchart@users.noreply.github.com>
2021-03-23 17:52:49 +0100

    Add files via upload

---
+++ TP17-CalculMAT(1)/TP17B.py
@@ -0,0 +1,46 @@
+﻿def somme(M,N):
+    nblignes = len(M)
+    nbcolonnes = len(M[0])
+    assert len(N) == len(M) and len(N[0]) == len(M[0])
+    resu=[ [0 for j in range (nbcolonnes)] for i in range (nblignes)]
+
+    for i in range(nblignes):
+        for j in range(nbcolonnes):
+            resu[i][j] = M[i][j] + N[i][j]
+    return resu
+
+def nombre_fois_tableau(k,M):
+    nblignes = len(M)
+    nbcolonnes = len(M[0])
+    resu=[ [0 for j in range (nbcolonnes)] for i in range (nblignes)]
+
+    for i in range(nblignes):
+    # pour chaque indice de ligne
+        for j in range(nbcolonnes):
+        # pour chaque indice de colonne
+            resu[i][j] = k * M[i][j]
+    return resu
+
+def difference(M,N):
+    oppN = nombre_fois_tableau(-1,N)
+    return somme(M, oppN)
+
+A =[[1,2,3],
+    [4,5,6],
+    [7,8,9]]
+
+B =[[1,4,7],
+    [2,5,8],
+    [3,6,9]]
+
+C =[[0,-4,70],
+    [20,15,88],
+    [-3,76,19]]
+
+D = [[1,2,3],
+     [4,5,6]]
+
+E =[[0,-4,70],
+    [20,15,88]]
+
+
